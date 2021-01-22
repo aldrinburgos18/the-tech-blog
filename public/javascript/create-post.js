c0b21a2672e04fef0ead8dd95d75defbc79ceb82
aldrinburgos <burgos.aldrin@gmail.com>
2021-01-21 19:25:39 -0600

    add templates and javascript

---
+++ public/javascript/create-post.js
@@ -0,0 +1,29 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_message = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
+
+  const response = await fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      post_message,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,33 @@
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const title = document.querySelector("#postTitle").value;
+  const post_message = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title,
+      post_message,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".edit-post-form")
+  .addEventListener("submit", editFormHandler);
